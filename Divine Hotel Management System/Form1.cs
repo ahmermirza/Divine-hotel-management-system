d384649938a934f43a9343c61c721ff0cf1755a6
ahmermirza <ahmermirzaonline@gmail.com>
2019-01-31 14:05:23 +0500

    2nd change

--- Divine Hotel Management System/Form1.cs
+++ Divine Hotel Management System/Form1.cs
@@ -16,5 +16,10 @@ namespace Divine_Hotel_Management_System
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //This is test
+        }
     }
 }
